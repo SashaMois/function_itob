60c4ec26a1168fdb55cb960bc5ee2a36c6d02641
SashaMois <56739595+SashaMois@users.noreply.github.com>
2020-02-23 19:35:00 +0300

    Create itob.c

---
+++ itob.c
@@ -0,0 +1,66 @@
+#include <string.h>
+#include <stdio.h>
+
+/*
+ * itob(): transforms integer to integer string with number system 'b'
+ */
+
+void itob(int number, char string[], int number_system);
+
+int main()
+{
+    char string[10000];
+    int n = 64;
+    itob(n, string, 2);
+
+    for (int i = 0; string[i] != '\0'; ++i)
+        putchar(string[i]);
+
+    return 0;
+}
+
+void itob(int n, char s[], int b)
+{
+    int power = 1;
+    int counter = 0;
+    int i;
+
+    while (n >= power)
+        power *= b;
+    power /= b;
+
+    for (i = 0; power > 0; ++i) {
+        while (n >= power) {
+            n -= power;
+            ++counter;
+        }
+
+        switch (counter) {
+        case 10:
+            s[i] = 'A';
+            break;
+        case 11:
+            s[i] = 'B';
+            break;
+        case 12:
+            s[i] = 'C';
+            break;
+        case 13:
+            s[i] = 'D';
+            break;
+        case 14:
+            s[i] = 'E';
+            break;
+        case 15:
+            s[i] = 'F';
+            break;
+        default:
+            s[i] = counter + '0';
+            break;
+        }
+        counter = 0;
+        power /= b;
+    }
+    s[i] = '\0';
+}
+
